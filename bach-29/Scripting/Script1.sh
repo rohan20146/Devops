5032a2a0941b1fa719a84e8c2653bf48100742ea
rohan20146 <ronakshetye203@gmail.com>
2025-09-03 14:28:51 +0530

    Some changes

--- bach-29/Scripting/Script1.sh
+++ bach-29/Scripting/Script1.sh
@@ -2,9 +2,8 @@ echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "I love "
 echo "We are learning AWS, Azure DevOps and DevSecOps
-echo "Hi I am Reyansh shetye"
 echo "Hi I am Rohan"
-echo "Hi I am Rakesh"
+echo "Hi I am Reyansh"
 echo "Hi I am Priya"
 
 
