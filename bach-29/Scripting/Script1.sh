f63458d92b72f2ee6ac343497975d5a9e4dd36f6
rshetye639-20 <rshetye639@gmail.com>
2026-03-12 16:05:17 +0530

    First

--- bach-29/Scripting/Script1.sh
+++ bach-29/Scripting/Script1.sh
@@ -3,8 +3,8 @@ echo "I am from Young minds"
 echo "I love "
 echo "We are learning AWS, Azure DevOps and DevSecOps
 echo "Hi I am Rohan"
-echo "Hi I am Reyansh"
-echo "Hi I am Priya"
+echo "my love Priya"
+echo "My jaan Reyansh"
 
 
 
